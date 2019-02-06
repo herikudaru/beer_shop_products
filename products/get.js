456f171a3c5be916e477c1102affbcc665ca6a11
Joakim Granqvist <granqvjo@arcada.fi>
2019-02-06 20:32:22 +0200

    added yml and made a hello world

--- products/get.js
+++ products/get.js
@@ -1,1 +1,9 @@
-//Function get script
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({"message": "Hello"}),
+    };
+    callback(null, response);
+};
