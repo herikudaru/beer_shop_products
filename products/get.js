9981d3970f0a884127722026d6e40def280eb932
Ben <lonnqvistben@gmail.com>
2019-02-17 18:35:18 +0200

    simple dynamodb scan

--- products/get.js
+++ products/get.js
@@ -1,35 +1,26 @@
 'use strict';
 
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
 module.exports.get = (event, context, callback) => {
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Key: {
-      id: event.pathParameters.id,
-    },
+
+  var AWS = require("aws-sdk");
+  
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  
+  var params = {
+      TableName: "Products",
   };
-
-  // fetch todo from the database
-  dynamoDb.get(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todo item.',
-      });
-      return;
-    }
-
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Item),
-    };
-    callback(null, response);
-  });
+  
+  docClient.scan(params, onScan);
+  
+  function onScan(err, data) {
+      if (err) {
+          callback(JSON.stringify(err, null, 2));
+      } else {
+          const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Items),
+          };
+          callback(null, response);
+      }
+  }
 };
