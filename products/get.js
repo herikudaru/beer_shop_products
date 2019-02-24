6c0ccc2c0403bd2e6c50e33549309ad5a3a43a9c
Lilian <kiuo88@gmail.com>
2019-02-24 18:37:28 +0200

    added possibility of getting a single item with error if item doesn't exist

--- products/get.js
+++ products/get.js
@@ -3,24 +3,46 @@
 module.exports.get = (event, context, callback) => {
 
   var AWS = require("aws-sdk");
-  
+
   var docClient = new AWS.DynamoDB.DocumentClient();
-  
+
   var params = {
-      TableName: "Products",
+    TableName: "Products",
   };
-  
-  docClient.scan(params, onScan);
-  
-  function onScan(err, data) {
+  var productid = event.headers.product_id || "";
+  if (productid !== "") {
+    var statusState = 200;
+
+    params["Key"] = { "id": productid }
+    docClient.get(params, function (err, data) {
       if (err) {
-          callback(JSON.stringify(err, null, 2));
+        callback(JSON.stringify(err, null, 2));
       } else {
-          const response = {
-            statusCode: 200,
-            body: JSON.stringify(data.Items),
-          };
-          callback(null, response);
+        if (JSON.stringify(data) === "{}") {
+          statusState = 404;
+        }
+        const response = {
+          statusCode: statusState,
+          body: statusState == 200 ? JSON.stringify(data) : null,
+        };
+        callback(null, response);
       }
+    });
+  }
+  else {
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
+      if (err) {
+        callback(JSON.stringify(err, null, 2));
+      } else {
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify(data.Items),
+        };
+        callback(null, response);
+      }
+    }
+
   }
 };
