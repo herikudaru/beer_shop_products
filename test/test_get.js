a690525421ec913e894880d4479b964fa70bee81
Claudia Mishima <claudia.mishima@gmail.com>
2019-03-03 16:21:51 +0200

    added initial get tester

---
+++ test/test_get.js
@@ -0,0 +1,18 @@
+const https = require('https');
+
+https.get('https://6uski1shah.execute-api.eu-north-1.amazonaws.com/dev/products', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
