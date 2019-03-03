a51c7334edaaefcbbfc79c08e9f7616c4ff28b9b
Claudia Mishima <claudia.mishima@gmail.com>
2019-03-03 17:11:11 +0200

    fixed mocha again

--- test/test_get.js
+++ test/test_get.js
@@ -2,19 +2,19 @@ const https = require('https');
 var chai = require('chai');
 var chaiHTTP = require('chai-http');
 
-const should = chai.should();
-const expect = chai.expect();
+var expect = chai.expect;
 
 chai.use(chaiHTTP);
 
 describe('GET routes', () => {
     describe('index', () => {
 
-        it('getting beer, its json', function() {   // <= No done callback
+        it('getting beer, its json', function(done) {   // <= No done callback
             chai.request('https://6uski1shah.execute-api.eu-north-1.amazonaws.com/dev/products')
             .get('')
-            .end(function(err, res) {
-                expect(req).to.be.json; // <= Test completes before this runs
+            .end(function(err, req) {
+                expect(req).to.be.json;
+                done(); // <= Test completes before this runs
             });
         });
 
