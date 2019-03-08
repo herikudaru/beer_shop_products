5f060df9894613755b67eb43affb10cf13e80150
Ben Lönnqvist <lonnqvistben@gmail.com>
2019-03-08 11:48:52 +0200

    ifx unauthorized access (#23)
    fix unauthorized access

--- products/get.js
+++ products/get.js
@@ -4,10 +4,14 @@ module.exports.get = (event, context, callback) => {
 
   const sharedSecretKey = process.env.SHARED_SECRET_KEY || "";
   const clientKey = event.headers.shared_secret_key || "";
-  if (sharedSecretKey == "")
+  if (sharedSecretKey == "") {
     callback('error');
-  if (clientKey == "" || clientKey != sharedSecretKey)
+    return;
+  }
+  if (clientKey == "" || clientKey != sharedSecretKey) {
     callback(null, {statusCode:401, body:JSON.stringify({"message":"unauthorized"})});
+    return;
+  }
 
   var AWS = require("aws-sdk");
 
