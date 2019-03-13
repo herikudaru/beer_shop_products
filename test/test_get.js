c4aee1f9338701df37dc9a20f1cad61a9fcb14b8
Buster Pihl <pihlbuster@gmail.com>
2019-03-13 22:21:29 +0200

    expected get 404

--- test/test_get.js
+++ test/test_get.js
@@ -14,6 +14,17 @@ describe('GET routes', () => {
             .get('')
             .end(function(err, req) {
                 expect(req).to.be.json;
+                expect(req).to.have.status(200);
+                done(); // <= Test completes before this runs
+            });
+        });
+
+        it('getting 1 beer, fails as expected', function(done) {   // <= No done callback
+            chai.request(process.env.TEST_API_ENDPOINT)
+            .get('')
+            .set('product_id', 156)
+            .end(function(err, req) {
+                expect(req).to.have.status(404);
                 done(); // <= Test completes before this runs
             });
         });
