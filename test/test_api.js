a4233f8d6279ab6cff8387f8146776f0bae372d9
Buster Pihl <pihlbuster@gmail.com>
2019-03-14 07:44:46 +0200

    removed unnecessary files

--- test/test_api.js
+++ test/test_api.js
@@ -84,25 +84,6 @@ describe('/products', () => {
         });
     });
 
-    /*it('it should fail to modify a product', function(done) {
-        chai.request(process.env.TEST_API_ENDPOINT)
-        .put('')
-        .set('shared_secret_key', process.env.shared_secret_key)
-        .set('product_id', prodId)
-        .send({
-            "name": "Modified beer",
-            "description": "Ale, 3.1%",
-            "image": "example.com/12_new_final_final_new.png",
-            "price": 10.99
-        }
-        )
-        .end(function(err, req) {
-            expect(req).to.have.status(502);
-            expect(req).to.be.json;
-            done();
-        });
-    });*/
-
     it('should modify a product', function(done) {
         chai.request(process.env.TEST_API_ENDPOINT)
         .put('')
