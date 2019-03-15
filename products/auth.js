37306db3e087cfe78f7ce1e099c41b214e86af1f
Ben <lonnqvist@gmail.com>
2019-03-15 13:07:03 +0200

    adds auth function

---
+++ products/auth.js
@@ -0,0 +1,26 @@
+module.exports.auth = (event) => {
+
+    // adds the function `getProp` to Object
+    // allowing for case insensitively getting keys 
+    Object.defineProperty(Object.prototype, "getProp", {
+        value: function (prop) {
+            var key,self = this;
+            for (key in self) {
+                if (key.toLowerCase() == prop.toLowerCase()) {
+                    return self[key];
+                }
+            }
+        },
+    });
+
+    const sharedSecretKey = process.env.SHARED_SECRET_KEY || "";
+    const clientKey = event.headers.getProp('shared_secret_key') || "";
+    
+    if (sharedSecretKey == "") {
+      return null;
+    }
+    if (clientKey == "" || clientKey != sharedSecretKey) {
+      return false;
+    }
+    return true;
+}
