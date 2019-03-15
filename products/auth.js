73c6e6cd2d75cd8d4a5ef0f116bd53e15ac5fe26
Ben Lönnqvist <lonnqvistben@gmail.com>
2019-03-15 14:30:15 +0200

    makes the shared key header key case insensitive (#29)
    * adds auth function
    
    * implements auth function
    
    * removed trailing comma
    
    * attempted fix
    
    * fix defineproperty

---
+++ products/auth.js
@@ -0,0 +1,28 @@
+module.exports.auth = (event) => {
+
+    // adds the function `getProp` to Object
+    // allowing for case insensitively getting keys 
+    Object.defineProperty(Object.prototype, "getProp", {
+        value: function (prop) {
+            var key,self = this;
+            for (key in self) {
+                if (key.toLowerCase() == prop.toLowerCase()) {
+                    return self[key];
+                }
+            }
+        },
+        enumerable: false,
+        writable: true
+    });
+
+    const sharedSecretKey = process.env.SHARED_SECRET_KEY || "";
+    var clientKey = event.headers.getProp('shared_secret_key') || "";
+    
+    if (sharedSecretKey == "") {
+      return null;
+    }
+    if (clientKey == "" || clientKey != sharedSecretKey) {
+      return false;
+    }
+    return true;
+}
